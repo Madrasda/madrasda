ec33dcc11f9bb319a7af02d0b6350d8a7f8b72ea
Alphacode14052004 <jimvenkat999@gmail.com>
2023-04-15 00:25:13 +0530

    Fixed the vendor login bug
    Co-authored-by: Adnan Mohamed <Dimitrov-Codes@users.noreply.github.com>

--- src/utils/JWTVerifier.js
+++ src/utils/JWTVerifier.js
@@ -1,3 +1,18 @@
-const isTokenValid(jwt){
-    
+import jwtDecode from 'jwt-decode';
+export const isTokenValid = (token) => {
+    try {
+        const decodedToken = jwtDecode(token);
+        console.log(decodedToken);
+        const currentTime = Date.now() / 1000; // divide by 1000 to convert to seconds
+        if (decodedToken.exp < currentTime) {
+            // token has expired
+            return false;
+        } else {
+            // token is valid
+            return true;
+        }
+    } catch (error) {
+        // error decoding token
+        return false;
+    }
 }
