5e55d0538637bed56afd14a88ea9dfa78dc3b886
majestymewtwo <majestymewtwo@gmail.com>
2023-04-26 09:45:30 +0530

    fixed client side bugs - 1

--- src/components/hotsellers-client.js
+++ src/components/hotsellers-client.js
@@ -8,51 +8,68 @@ export default function HotSellers() {
     const [products, setProducts] = useState([]);
 
     const getAllProducts = async () => {
-    const response = await axios.get(
-      "https://spring-madrasda-2f6mra4vwa-em.a.run.app/api/client/allProducts?pageNo=0&pageSize=4"
-    );
-    setProducts(response.data.content);
-  }
+      const response = await axios.get(
+        "https://spring-madrasda-2f6mra4vwa-em.a.run.app/api/product/hotsellers"
+      );
+      setProducts(response.data);
+    };
 
-  useEffect(() => {
-    getAllProducts();
-  }, []);
+    useEffect(() => {
+      getAllProducts();
+    }, []);
 
     return (
-        <section className="text-black body-font">
-        {    products &&
-            <div className='p-10 font-algeria'>
-                <div className='border-black border-2 rounded-xl scale-[90%]'>
-                    <div className='relative'>
-                        <h2 className='font-algeria font-semibold text-xl flex justify-center items-center w-full
+      <section className='text-black body-font'>
+        {products && (
+          <div className='p-10 font-algeria'>
+            <div className='border-black border-2 rounded-xl scale-[90%]'>
+              <div className='relative'>
+                <h2
+                  className='font-algeria font-semibold text-xl flex justify-center items-center w-full
                       m-0 absolute top-[50%] left-0 right-0 bottom-0 -z-1'>
-                        <span className='bg-white px-8 py-4 rounded-full text-black'>
-                          HOTSELLERS
-                        </span>
-                        </h2>
-                    </div>
-                </div>
-            </div>}
-            <div className="flex justify-center">
-                {products && products.map((product) => {
-                         return (
-                              <Link key={uuidv4()} href={`/productDetails/${product.id}`} className="lg:w-1/4 md:w-1/2 p-4 w-full cursor-pointer bg-off-white mx-4 my-2 rounded drop-shadow-[8px_8px_10px_rgba(0,0,0,0.3)] hover:drop-shadow-[8px_8px_4px_rgba(0,0,0,0.4)] duration-300 ease-in-out">
-                                 <div className="block relative h-48 rounded overflow-hidden">
-                                     <Image src={product.colors[0].images[0]} alt="ecommerce" width={1080} height={1920} className="object-contain object-center w-full h-full block" />
-                                 </div>
-                                 <div className="mt-4">
-                                     <h2 className="title-font text-base font-medium">{product.name}</h2>
-                                     <span className="mt-1 text-black text-lg pr-1">
-                                         ₹{Math.round(product.total -(product.total*product.discount*0.01))}
-                                     </span>
-                                     <span className="mt-1 line-through text-gray pr-1">₹{product.total}</span>
-                                     <span className="title-font text-xs font-medium text-[#088240]">{product.discount}% OFF</span>
-                                 </div>
-                             </Link>
-                         )
-                     })
-                }
+                  <span className='bg-white px-8 py-4 rounded-full text-black'>
+                    HOTSELLERS
+                  </span>
+                </h2>
+              </div>
             </div>
-        </section>
-    )
+          </div>
+        )}
+        <div className='flex flex-wrap justify-center'>
+          {products &&
+            products.map((product) => {
+              return (
+                <Link
+                  key={uuidv4()}
+                  href={`/productDetails/${product.id}`}
+                  className='lg:w-1/4 md:w-1/2 p-4 w-full cursor-pointer bg-off-white mx-4 my-2 rounded drop-shadow-[8px_8px_10px_rgba(0,0,0,0.3)] hover:drop-shadow-[8px_8px_4px_rgba(0,0,0,0.4)] duration-300 ease-in-out'>
+                  <div className='block relative h-48 rounded overflow-hidden'>
+                    <Image
+                      src={product.colors[0].images[0]}
+                      alt='ecommerce'
+                      width={1080}
+                      height={1920}
+                      className='object-contain object-center w-full h-full block'
+                    />
+                  </div>
+                  <div className='mt-4'>
+                    <h2 className='title-font text-base font-medium'>
+                      {product.name}
+                    </h2>
+                    <span className='mt-1 text-black text-lg pr-1'>
+                      ₹{Math.round(product.total)}
+                    </span>
+                    <span className='mt-1 line-through text-gray pr-1'>
+                      ₹{product.total}
+                    </span>
+                    <span className='title-font text-xs font-medium text-[#088240]'>
+                      {product.discount}% OFF
+                    </span>
+                  </div>
+                </Link>
+              );
+            })}
+        </div>
+      </section>
+    );
 }
--- src/components/orderdetails-modal.js
+++ src/components/orderdetails-modal.js
@@ -13,82 +13,94 @@ export default function OrderDetailsModal({order}) {
     };
 
     return (
-        <div>
-            <Button
-                auto
-                onPress={handler}
-                color='error'
-                style={{
-                    background: "#A5153F",
-                }}>
-                Order Details
-            </Button>
-            <Modal
-                width='1080px'
-                closeButton
-                preventClose
-                aria-labelledby='modal-title'
-                open={visible}
-                onClose={closeHandler}
-                css={{fontFamily: "$algeria"}}>
-                <Modal.Header css={{fontFamily: "$algeria"}}>
-                    <Text id='modal-title' size={18}>
-                        Billing Information
-                    </Text>
-                </Modal.Header>
+      <div>
+        <Button
+          auto
+          onPress={handler}
+          color='error'
+          style={{
+            background: "#A5153F",
+          }}>
+          Order Details
+        </Button>
+        <Modal
+          width='1080px'
+          closeButton
+          preventClose
+          aria-labelledby='modal-title'
+          open={visible}
+          onClose={closeHandler}
+          css={{ fontFamily: "$algeria" }}>
+          <Modal.Header css={{ fontFamily: "$algeria" }}>
+            <Text id='modal-title' size={18}>
+              Billing Information
+            </Text>
+          </Modal.Header>
 
-                <Modal.Body css={{fontFamily: "$algeria"}}>
-                    <div className='bg-white shadow-lg rounded-lg overflow-hidden w-full md:w-3/4 mx-auto p-4'>
-                        {/* <!-- Order header --> */}
-                        <div className='bg-bg text-white py-4 px-6 flex justify-between items-center'>
-                            <h1 className='text-xl font-semibold'>Order #{order.id}</h1>
-                            <span className='text-lg'>
-                ₹{Number(order.orderTotal).toLocaleString("en-IN")}
-              </span>
-                        </div>
-                        {/* <!-- Order details --> */}
-                        <div className='p-6'>
-                            {order.orderItems.map((item) => (
-                                <div key={uuidv4()} className='flex justify-between items-center mb-4'>
-                                    <span className='text-lg font-medium'>{item.product.name}</span>
-                                    <span className='text-black'>
-                    ₹
-                                        {Number(item.product.total * item.quantity).toLocaleString(
-                                            "en-IN"
-                                        )}
-                  </span>
-                                </div>
-                            ))}
-                            <hr className='my-6'/>
-                            {/* <!-- Billing information --> */}
-                            <h2 className='text-lg font-medium mb-4'>Shipping Address</h2>
-                            <div className='grid grid-cols-2 gap-4'>
-                                <div>
-                                    <span className='text-black'>Name</span>
-                                    <p className='font-medium'>{order.shippingAddress.name}</p>
-                                </div>
-                                <div>
-                                    <span className='text-black'>Email</span>
-                                    <p className='font-medium'>{order.shippingAddress.email}</p>
-                                </div>
-                                <div>
-                                    <span className='text-black'>Address</span>
-                                    <p className='font-medium'>
-                                        {order.shippingAddress.addressLine1}
-                                    </p>
-                                    <p className='font-medium'>
-                                        {order.shippingAddress.addressLine2}
-                                    </p>
-                                    <p className='font-medium'>{order.shippingAddress.city}</p>
-                                    <p className='font-medium'>{order.shippingAddress.state}</p>
-                                    <p className='font-medium'>{order.shippingAddress.postalCode}</p>
-                                    <p className='font-medium'>{order.shippingAddress.country}</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </Modal.Body>
-            </Modal>
-        </div>
+          <Modal.Body css={{ fontFamily: "$algeria" }}>
+            <div className='bg-white shadow-lg rounded-lg overflow-hidden w-full md:w-3/4 mx-auto p-4'>
+              {/* <!-- Order header --> */}
+              <div className='bg-bg text-white py-4 px-6 flex justify-between items-center'>
+                <h1 className='text-xl font-semibold'>Order #{order.id}</h1>
+                <span className='text-lg'>
+                  ₹{Number(order.orderTotal).toLocaleString("en-IN")}
+                </span>
+              </div>
+              {/* <!-- Order details --> */}
+              <div className='p-6'>
+                {order.orderItems.map((item) => (
+                  <div
+                    key={uuidv4()}
+                    className='flex justify-between items-center mb-4'>
+                    <span className='text-lg font-medium'>
+                      {item.product.name}
+                    </span>
+                    <span className='text-black'>
+                      ₹
+                      {Number(
+                        item.product.total * item.quantity
+                      ).toLocaleString("en-IN")}
+                    </span>
+                  </div>
+                ))}
+                <hr className='my-6' />
+                {/* <!-- Billing information --> */}
+                <div className='flex justify-between'>
+                  <h1 className='text-lg font-medium mb-4'>Payment ID</h1>
+                  <h1 className='text-gray'>{order.paymentId}</h1>
+                </div>
+                <h2 className='text-lg font-medium mb-4'>Shipping Address</h2>
+                <div className='grid grid-cols-2 gap-4'>
+                  <div>
+                    <span className='text-black'>Name</span>
+                    <p className='font-medium'>{order.shippingAddress.name}</p>
+                  </div>
+                  <div>
+                    <span className='text-black'>Email</span>
+                    <p className='font-medium'>{order.shippingAddress.email}</p>
+                  </div>
+                  <div>
+                    <span className='text-black'>Address</span>
+                    <p className='font-medium'>
+                      {order.shippingAddress.addressLine1}
+                    </p>
+                    <p className='font-medium'>
+                      {order.shippingAddress.addressLine2}
+                    </p>
+                    <p className='font-medium'>{order.shippingAddress.city}</p>
+                    <p className='font-medium'>{order.shippingAddress.state}</p>
+                    <p className='font-medium'>
+                      {order.shippingAddress.postalCode}
+                    </p>
+                    <p className='font-medium'>
+                      {order.shippingAddress.country}
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Modal.Body>
+        </Modal>
+      </div>
     );
 }
