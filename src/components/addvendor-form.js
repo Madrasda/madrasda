b23125adc63e51e122e72b156bf0c6f6a35c73bf
majestymewtwo <majestymewtwo@gmail.com>
2023-05-06 21:14:36 +0530

    changed add vendor modal color

--- src/components/addvendor-form.js
+++ src/components/addvendor-form.js
@@ -29,49 +29,70 @@ export default function AddVendorForm({onSubmit}) {
 
   return (
     <form onSubmit={handleSubmit}>
-        <h2 className="ml-2  title-font font-medium text-sm">Vendor Display Name :</h2>
-        <div className="mb-2 ml-2 ">
-            <input type="text" className="bg-white border border-[#D9D9D9] text-black text-sm rounded-lg focus:ring-primary focus:border-[#D9D9D9] block w-full p-2.5" placeholder="Enter your Display Name" onChange={
-              (e) => setName(e.target.value)
-            } />
-        </div>
-        <h2 className="ml-2  title-font font-medium text-sm">Vendor Email :</h2>
-        <div className="mb-2 ml-2 ">
-            <input type="text" className="bg-white border border-[#D9D9D9] text-black text-sm rounded-lg focus:ring-primary focus:border-[#D9D9D9] block w-full p-2.5" placeholder="Enter your Email Address" onChange={
-              (e) => setEmail(e.target.value)
-            } />
-        </div>
-        <h2 className="ml-2  title-font font-medium text-sm">Company Name :</h2>
-        <div className="mb-2 ml-2 ">
-            <input type="text" className="bg-white border border-[#D9D9D9] text-black text-sm rounded-lg focus:ring-primary focus:border-[#D9D9D9] block w-full p-2.5" placeholder="Enter your Company Name" onChange={
-              (e) => setCompanyName(e.target.value)
-            }/>
-        </div>
-        <h2 className="ml-2  title-font font-medium text-sm">Company URL :</h2>
-        <div className="mb-2 ml-2">
-            <input type="text" className="bg-white border border-[#D9D9D9] text-black text-sm rounded-lg focus:ring-primary focus:border-[#D9D9D9] block w-full p-2.5" placeholder="www.example.com" onChange={
-              (e) => setCompanyUrl(e.target.value)
-            }/>
-        </div>
-        <h2 className="ml-2 title-font font-medium text-sm">GST IN :</h2>
-        <div className="mb-2 ml-2">
-            <input type="text" className="bg-white border border-[#D9D9D9] text-black text-sm rounded-lg focus:ring-primary focus:border-[#D9D9D9] block w-full p-2.5" placeholder="33XXXXXXXXXXXTN1X" onChange={
-              (e) => setGst(e.target.value)
-            }/>
-        </div>
-        <h2 className="ml-2 title-font font-medium text-sm">Default Password</h2>
-        <div className="mb-2 ml-2">
-            <input type="text" className="bg-white border border-[#D9D9D9] text-black text-sm rounded-lg focus:ring-primary focus:border-[#D9D9D9] block w-full p-2.5" placeholder="Password" onChange={
-              (e) => setPassword(e.target.value)
-            }/>
-        </div>
-        <Button auto
-          type="submit"
-          style={{
-            background: "#A5153F",
-          }}>
-            Add Vendor
+      <h2 className='ml-2  title-font font-medium text-sm'>
+        Vendor Display Name :
+      </h2>
+      <div className='mb-2 ml-2 '>
+        <input
+          type='text'
+          className='bg-white border border-[#D9D9D9] text-black text-sm rounded-lg focus:ring-primary focus:border-[#D9D9D9] block w-full p-2.5'
+          placeholder='Enter your Display Name'
+          onChange={(e) => setName(e.target.value)}
+        />
+      </div>
+      <h2 className='ml-2  title-font font-medium text-sm'>Vendor Email :</h2>
+      <div className='mb-2 ml-2 '>
+        <input
+          type='text'
+          className='bg-white border border-[#D9D9D9] text-black text-sm rounded-lg focus:ring-primary focus:border-[#D9D9D9] block w-full p-2.5'
+          placeholder='Enter your Email Address'
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <h2 className='ml-2  title-font font-medium text-sm'>Company Name :</h2>
+      <div className='mb-2 ml-2 '>
+        <input
+          type='text'
+          className='bg-white border border-[#D9D9D9] text-black text-sm rounded-lg focus:ring-primary focus:border-[#D9D9D9] block w-full p-2.5'
+          placeholder='Enter your Company Name'
+          onChange={(e) => setCompanyName(e.target.value)}
+        />
+      </div>
+      <h2 className='ml-2  title-font font-medium text-sm'>Company URL :</h2>
+      <div className='mb-2 ml-2'>
+        <input
+          type='text'
+          className='bg-white border border-[#D9D9D9] text-black text-sm rounded-lg focus:ring-primary focus:border-[#D9D9D9] block w-full p-2.5'
+          placeholder='www.example.com'
+          onChange={(e) => setCompanyUrl(e.target.value)}
+        />
+      </div>
+      <h2 className='ml-2 title-font font-medium text-sm'>GST IN :</h2>
+      <div className='mb-2 ml-2'>
+        <input
+          type='text'
+          className='bg-white border border-[#D9D9D9] text-black text-sm rounded-lg focus:ring-primary focus:border-[#D9D9D9] block w-full p-2.5'
+          placeholder='33XXXXXXXXXXXTN1X'
+          onChange={(e) => setGst(e.target.value)}
+        />
+      </div>
+      <h2 className='ml-2 title-font font-medium text-sm'>Default Password</h2>
+      <div className='mb-2 ml-2'>
+        <input
+          type='text'
+          className='bg-white border border-[#D9D9D9] text-black text-sm rounded-lg focus:ring-primary focus:border-[#D9D9D9] block w-full p-2.5'
+          placeholder='Password'
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <div className="flex justify-center">
+        <Button
+          auto
+          type='submit'
+          className='bg-primary text-white hover:bg-logo'>
+          Add Vendor
         </Button>
+      </div>
     </form>
-  )
+  );
 }
--- src/components/addvendor-modal.js
+++ src/components/addvendor-modal.js
@@ -3,80 +3,84 @@ import { css, Modal, Button, Text } from "@nextui-org/react";
 import Image from "next/image";
 import WithdrawForm from "./withdraw-form";
 import AddVendorForm from "./addvendor-form";
+import { SupervisedUserCircleRounded } from "@mui/icons-material";
 
-export default function AddVendorModal({getFormData}) {
+export default function AddVendorModal({ getFormData }) {
   const [visible, setVisible] = useState(false);
   const [image, setImage] = useState(null);
 
   const handleSubmit = (data) => {
     getFormData({
       ...data,
-      imgUrl : image
+      imgUrl: image,
     });
     closeHandler();
-  }
+  };
 
   const handler = () => setVisible(true);
   const closeHandler = () => {
     setVisible(false);
+    setImage(null);
   };
   return (
     <div>
-      <Button auto ghost onPress={handler}
+      <Button
+        auto
+        ghost
+        onPress={handler}
         className='z-0'
         style={{
           background: "white",
           border: "white",
           z: 0,
-        }}
-        >
-        <Image src="/plus-icon.png" width={40} height={40}/>
-        </Button>
+        }}>
+        <Image src='/plus-icon.png' width={40} height={40} />
+      </Button>
       <Modal
         closeButton
         preventClose
-        aria-labelledby="modal-title"
+        aria-labelledby='modal-title'
         open={visible}
         onClose={closeHandler}
-        css={{fontFamily: '$algeria'}}
-      >
+        css={{ fontFamily: "$algeria" }}>
         <Modal.Header>
-          <Text id="modal-title" size={18} style={{
-          color: "#A5153F",
-        }}>
+          <Text id='modal-title' size={18} className='text-primary font-quest'>
             Add Vendor
           </Text>
         </Modal.Header>
 
-        <Modal.Body css={{fontFamily: '$algeria'}}>
-        <label for="dropzone-file" className="flex items-center justify-center w-full h-fit bg-white">
-            <div className="flex flex-col items-center justify-center px-4 relative group transition-all duration-300 ease-in-out">
+        <Modal.Body className='font-quest'>
+          <label
+            for='dropzone-file'
+            className='flex items-center justify-center w-full h-fit bg-white'>
+            <div className='flex flex-col items-center justify-center px-4 relative group transition-all duration-300 ease-in-out'>
               {/* <h4 className="text-base">Upload</h4> */}
-              <div className="">
-              <Image src="/profile-logo.png" 
-                  alt="ecommerce" 
-                  height={60}
-                  width={60} 
-                  className="object-contain rounded-full cursor-pointer" />
+              <div className=''>
+                <SupervisedUserCircleRounded className='text-primary h-[70px] w-[70px]' />
               </div>
-              <div className="absolute z-20 hidden group-hover:block">
-              <Image src="/hover profile logo.png" 
-                  alt="ecommerce" 
+              <div className='absolute z-20 hidden group-hover:block'>
+                <Image
+                  src='/hover profile logo.png'
+                  alt='ecommerce'
                   height={60}
-                  width={60} 
-                  className="object-contain rounded-full cursor-pointer" />
+                  width={60}
+                  className='object-contain rounded-full cursor-pointer'
+                />
               </div>
             </div>
-            <input id="dropzone-file" type="file" className="hidden" onChange={(e) => {setImage(e.target.files[0])}} accept="image/jpeg" />
+            <input
+              id='dropzone-file'
+              type='file'
+              className='hidden'
+              onChange={(e) => {
+                setImage(e.target.files[0]);
+              }}
+              accept='image/jpeg'
+            />
             {image && <h1>{image.name}</h1>}
-        </label>
-            <AddVendorForm onSubmit={handleSubmit} />
+          </label>
+          <AddVendorForm onSubmit={handleSubmit} />
         </Modal.Body>
-        <Modal.Footer css={{fontFamily: '$algeria'}}>
-          <Button auto flat color="error" onPress={closeHandler}>
-            Close
-          </Button>
-        </Modal.Footer>
       </Modal>
     </div>
   );
