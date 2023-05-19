3f409a8b413d5f54b14d54d62101bf7891ac6952
majestymewtwo <majestymewtwo@gmail.com>
2023-05-19 23:20:05 +0530

    added phone number field to admin add vendor form

--- src/components/addvendor-form.js
+++ src/components/addvendor-form.js
@@ -5,6 +5,7 @@ import { Button } from "@mui/material";
 export default function AddVendorForm({onSubmit}) {
   const [name, setName] = useState(null);
   const [email, setEmail] = useState(null);
+  const [phone, setPhone] = useState(null);
   const [companyName, setCompanyName] = useState(null);
   const [companyUrl, setCompanyUrl] = useState(null);
   const [gst, setGst] = useState(null);
@@ -13,25 +14,22 @@ export default function AddVendorForm({onSubmit}) {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    onSubmit(
-      {
-        name: name,
-        email: email,
-        imgUrl: "",
-        password: password,
-        companyName: companyName,
-        gstin: gst,
-        companyUrl: companyUrl,
-        category: category
-      }
-    )
-  }
+    onSubmit({
+      name: name,
+      email: email,
+      phone: phone,
+      imgUrl: "",
+      password: password,
+      companyName: companyName,
+      gstin: gst,
+      companyUrl: companyUrl,
+      category: category,
+    });
+  };
 
   return (
     <form onSubmit={handleSubmit}>
-      <h2 className='ml-2  title-font font-medium text-sm'>
-        Display Name :
-      </h2>
+      <h2 className='ml-2  title-font font-medium text-sm'>Display Name :</h2>
       <div className='mb-2 ml-2 '>
         <input
           type='text'
@@ -49,6 +47,15 @@ export default function AddVendorForm({onSubmit}) {
           onChange={(e) => setEmail(e.target.value)}
         />
       </div>
+      <h2 className='ml-2  title-font font-medium text-sm'>Phone Number :</h2>
+      <div className='mb-2 ml-2 '>
+        <input
+          type='text'
+          className='bg-white border border-[#D9D9D9] text-black text-sm rounded-lg focus:ring-primary focus:border-[#D9D9D9] block w-full p-2.5'
+          placeholder='Enter your phone'
+          onChange={(e) => setPhone(e.target.value)}
+        />
+      </div>
       <h2 className='ml-2  title-font font-medium text-sm'>Creator Name :</h2>
       <div className='mb-2 ml-2 '>
         <input
@@ -58,7 +65,9 @@ export default function AddVendorForm({onSubmit}) {
           onChange={(e) => setCompanyName(e.target.value)}
         />
       </div>
-      <h2 className='ml-2  title-font font-medium text-sm'>Company/Creator URL :</h2>
+      <h2 className='ml-2  title-font font-medium text-sm'>
+        Company/Creator URL :
+      </h2>
       <div className='mb-2 ml-2'>
         <input
           type='text'
@@ -72,7 +81,8 @@ export default function AddVendorForm({onSubmit}) {
         <input
           type='text'
           className='bg-white border border-[#D9D9D9] text-black text-sm rounded-lg focus:ring-primary focus:border-[#D9D9D9] block w-full p-2.5'
-          placeholder='33XXXXXXXXXXXTN1X' maxLength={15}
+          placeholder='33XXXXXXXXXXXTN1X'
+          maxLength={15}
           onChange={(e) => setGst(e.target.value)}
         />
       </div>
@@ -85,17 +95,17 @@ export default function AddVendorForm({onSubmit}) {
           onChange={(e) => setPassword(e.target.value)}
         />
       </div>
-      <div className="flex justify-center">
-      <Button /*{className='bg-primary text-white mx-auto w-full ' type='submit'}*/ 
-        css={{ fontFamily: "$algeria" }}
-        style={{
-          background: "#FFA000",
-          color: "white"
-        }}
-        type={'submit'}
-        className={`font-bold text-white py-2 px-4 mr-2 mb-2`}>
-        Add vendor
-      </Button>
+      <div className='flex justify-center'>
+        <Button /*{className='bg-primary text-white mx-auto w-full ' type='submit'}*/
+          css={{ fontFamily: "$algeria" }}
+          style={{
+            background: "#FFA000",
+            color: "white",
+          }}
+          type={"submit"}
+          className={`font-bold text-white py-2 px-4 mr-2 mb-2`}>
+          Add vendor
+        </Button>
       </div>
     </form>
   );
