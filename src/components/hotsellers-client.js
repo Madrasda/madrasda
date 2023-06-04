98dc7238320312fa5bad62848ec7d1741b28ac56
Kamalesh <kamalesh2023@gmail.com>
2023-06-04 14:53:11 +0530

    Price rounding bugs pt-3

--- src/components/hotsellers-client.js
+++ src/components/hotsellers-client.js
@@ -1,77 +1,82 @@
 import axios from "axios";
-import Image from "next/image"
+import Image from "next/image";
 import Link from "next/link";
-import { useEffect, useState } from "react"
-import {uuidv4} from "@firebase/util";
+import { useEffect, useState } from "react";
+import { uuidv4 } from "@firebase/util";
 
 export default function HotSellers() {
-    const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState([]);
 
-    const getAllProducts = async () => {
-      const response = await axios.get(
-        "https://spring-madrasda-2f6mra4vwa-em.a.run.app/api/product/hotsellers"
-      );
-      setProducts(response.data);
-    };
+  const getAllProducts = async () => {
+    const response = await axios.get(
+      "https://spring-madrasda-2f6mra4vwa-em.a.run.app/api/product/hotsellers"
+    );
+    setProducts(response.data);
+  };
 
-    useEffect(() => {
-      getAllProducts();
-    }, []);
+  useEffect(() => {
+    getAllProducts();
+  }, []);
 
-    return (
-      <section className='text-black body-font'>
-        {products && (
-          <div className='p-0 md:p-10 font-quest'>
-            <div className='xl:border-shadowGrey md:border-[3px] rounded-xl scale-[90%]'>
-              <div className='relative'>
-                <h2
-                  className='font-raj font-semibold text-xl flex justify-center items-center w-full
-                      m-0 absolute top-1.5 md:top-[50%] left-0 right-0 bottom-0 -z-1'>
-                  <span className='bg-bg tracking-widest font-raj text-xl md:text-4xl font-bold px-8 py-2 xl:py-4 rounded-full text-logo'>
-                    HOTSELLERS
-                  </span>
-                </h2>
-              </div>
-              <div className='grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 justify-items-center mx-auto py-6'>
-                {products &&
-                  products.map((product) => {
-                    return (
-                      <Link
-                        key={uuidv4()}
-                        href={`/productDetails/${product.id}`}
-                        className='col-span-1 w-40 md:w-56 xl:w-60 p-2 xl:p-4 h-fit cursor-pointer bg-off-white mx-auto my-3 md:my-4 xl:my-5 rounded
+  return (
+    <section className="text-black body-font">
+      {products && (
+        <div className="p-0 md:p-10 font-quest">
+          <div className="xl:border-shadowGrey md:border-[3px] rounded-xl scale-[90%]">
+            <div className="relative">
+              <h2
+                className="font-raj font-semibold text-xl flex justify-center items-center w-full
+                      m-0 absolute top-1.5 md:top-[50%] left-0 right-0 bottom-0 -z-1"
+              >
+                <span className="bg-bg tracking-widest font-raj text-xl md:text-4xl font-bold px-8 py-2 xl:py-4 rounded-full text-logo">
+                  HOTSELLERS
+                </span>
+              </h2>
+            </div>
+            <div className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 justify-items-center mx-auto py-6">
+              {products &&
+                products.map((product) => {
+                  return (
+                    <Link
+                      key={uuidv4()}
+                      href={`/productDetails/${product.id}`}
+                      className="col-span-1 w-40 md:w-56 xl:w-60 p-2 xl:p-4 h-fit cursor-pointer bg-off-white mx-auto my-3 md:my-4 xl:my-5 rounded
                         drop-shadow-[4px_4px_10px_rgba(0,0,0,0.2)] hover:drop-shadow-[8px_8px_4px_rgba(0,0,0,0.3)]
-                        duration-200 ease-in-out'>
-                        <div className='block relative h-fit rounded overflow-hidden'>
-                          <Image
-                            src={product.colors[0].images[0]}
-                            alt='ecommerce'
-                            width={1080}
-                            height={1920}
-                            className='object-contain object-center w-full h-[130px] xl:h-[296px] block'
-                          />
-                        </div>
-                        <div className='flex flex-col mt-4 font-quest'>
-                          <h2 className='title-font text-base md:text-xl font-medium'>
-                            {product.name}
-                          </h2>
-                          <span className='text-black text-lg'>
-                            ₹{(product.total * (100 - product.discount)) / 100}
-                          </span>
-                          <span className='line-through text-bg'>
-                            ₹{product.total}
-                          </span>
-                          <span className='title-font text-xs font-medium text-[#088240]'>
-                            {product.discount}% OFF
-                          </span>
-                        </div>
-                      </Link>
-                    );
-                  })}
-              </div>
+                        duration-200 ease-in-out"
+                    >
+                      <div className="block relative h-fit rounded overflow-hidden">
+                        <Image
+                          src={product.colors[0].images[0]}
+                          alt="ecommerce"
+                          width={1080}
+                          height={1920}
+                          className="object-contain object-center w-full h-[130px] xl:h-[296px] block"
+                        />
+                      </div>
+                      <div className="flex flex-col mt-4 font-quest">
+                        <h2 className="title-font text-base md:text-xl font-medium">
+                          {product.name}
+                        </h2>
+                        <span className="text-black text-lg">
+                          ₹
+                          {Math.round(
+                            (product.total * (100 - product.discount)) / 100
+                          )}
+                        </span>
+                        <span className="line-through text-bg">
+                          ₹{product.total}
+                        </span>
+                        <span className="title-font text-xs font-medium text-[#088240]">
+                          {product.discount}% OFF
+                        </span>
+                      </div>
+                    </Link>
+                  );
+                })}
             </div>
           </div>
-        )}
-      </section>
-    );
+        </div>
+      )}
+    </section>
+  );
 }
--- src/pages/ProductTile.js
+++ src/pages/ProductTile.js
@@ -1,46 +1,47 @@
 import Image from "next/image";
-import {useRouter} from "next/router";
+import { useRouter } from "next/router";
 
-const ProductTile = ({id, name, category, total, discount, imageUrl}) => {
-    const router = useRouter();
-    function viewProduct(id) {
-        router.push("/productDetails/" + id)
-    }
+const ProductTile = ({ id, name, category, total, discount, imageUrl }) => {
+  const router = useRouter();
+  function viewProduct(id) {
+    router.push("/productDetails/" + id);
+  }
 
-    return (
-      <div
-        onClick={() => viewProduct(id)}
-        className='col-span-1 p-3 w-[170px] md:w-[300px] md:h-fit cursor-pointer bg-off-white mx-2 md:mx-5 my-3 rounded-md border border-border
-        shadow-md hover:shadow-shadowGrey hover:shadow-lg transition-shadow'>
-        <a className='block relative h-[200px] md:h-fit rounded overflow-hidden'>
-          <Image
-            src={imageUrl}
-            alt='ecommerce'
-            height={1080}
-            width={1920}
-            className='object-contain object-center w-fit h-[200px] md:h-[300px] mx-auto'
-          />
-        </a>
-        <div className='mt-4'>
-          <div className='flex flex-row items-center w-full'>
-            <div className='flex justify-start'>
-              <h3 className='text-xs tracking-widest title-font mb-1 text-shadowGrey'>
-                {category}
-              </h3>
-            </div>
+  return (
+    <div
+      onClick={() => viewProduct(id)}
+      className="col-span-1 p-3 w-[170px] md:w-[300px] md:h-fit cursor-pointer bg-off-white mx-2 md:mx-5 my-3 rounded-md border border-border
+        shadow-md hover:shadow-shadowGrey hover:shadow-lg transition-shadow"
+    >
+      <a className="block relative h-[200px] md:h-fit rounded overflow-hidden">
+        <Image
+          src={imageUrl}
+          alt="ecommerce"
+          height={1080}
+          width={1920}
+          className="object-contain object-center w-fit h-[200px] md:h-[300px] mx-auto"
+        />
+      </a>
+      <div className="mt-4">
+        <div className="flex flex-row items-center w-full">
+          <div className="flex justify-start">
+            <h3 className="text-xs tracking-widest title-font mb-1 text-shadowGrey">
+              {category}
+            </h3>
           </div>
-          <h2 className='title-font text-lg md:text-xl font-medium'>{name}</h2>
-          <span className='mt-1 text-black pr-1'>
-            ₹{(total * (100 - discount)) / 100}
-          </span>
-          <span className='mt-1 line-through text-shadowGrey pr-1'>
-            ₹{total}
-          </span>
-          <span className='title-font text-xs font-medium text-[#088240]'>
-            {discount}% OFF
-          </span>
         </div>
+        <h2 className="title-font text-lg md:text-xl font-medium">{name}</h2>
+        <span className="mt-1 text-black pr-1">
+          ₹{Math.round((total * (100 - discount)) / 100)}
+        </span>
+        <span className="mt-1 line-through text-shadowGrey pr-1">
+          ₹{Math.round(total)}
+        </span>
+        <span className="title-font text-xs font-medium text-[#088240]">
+          {discount}% OFF
+        </span>
       </div>
-    );
-}
-export default ProductTile
+    </div>
+  );
+};
+export default ProductTile;
