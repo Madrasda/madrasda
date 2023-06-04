430e82a1625cfbebd9e84d3d62ea4ad0d51fed9b
Kamalesh <kamalesh2023@gmail.com>
2023-06-04 18:30:45 +0530

    Price rounding bugs pt-4

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
-import {useContext, useState} from "react";
-import {UserContext} from "../../context/context";
+import { useContext, useState } from "react";
+import { UserContext } from "../../context/context";
 import { IconButton } from "@mui/material";
 import { Add, Delete, Remove } from "@mui/icons-material";
 
@@ -13,33 +13,33 @@ const CartItem = ({ id, product, qty }) => {
     ctx.customQuantity(id, event.target.value);
   const removeItem = () => ctx.removeItem(id);
   return (
-    <div className='w-full md:w-auto flex flex-col md:flex-row items-center p-7 relative rounded bg-white font-quest'>
-      <div className='overflow-hidden rounded-lg w-56 h-auto border border-gray'>
+    <div className="w-full md:w-auto flex flex-col md:flex-row items-center p-7 relative rounded bg-white font-quest">
+      <div className="overflow-hidden rounded-lg w-56 h-auto border border-gray">
         <Image
           src={product.frontImage.imageUrl}
-          alt='ecommerce'
+          alt="ecommerce"
           width={1080}
           height={1920}
-          className='object-contain md:object-cover h-auto'
+          className="object-contain md:object-cover h-auto"
         />
       </div>
-      <div className='md:pl-3 md:ml-4 mb-4 text-sm md:text-base relative w-full'>
-        <div className='flex flex-col pt-2'>
-          <h6 className='font-medium text-lg md:text-2xl text-black'>
+      <div className="md:pl-3 md:ml-4 mb-4 text-sm md:text-base relative w-full">
+        <div className="flex flex-col pt-2">
+          <h6 className="font-medium text-lg md:text-2xl text-black">
             {product.name}
           </h6>
-          <p className='text-black'>{product.colorDTO.color}</p>
-          <p className='text-black'>Size-{product.sizeDTO.size}</p>
-          <div className='flex w-full objects-center'>
-            <div className='flex justify-between items-center mt-2 md:mt-4 h-4 w-16'>
-              <div className='flex h-10 w-fit bg-transparent mt-1'>
+          <p className="text-black">{product.colorDTO.color}</p>
+          <p className="text-black">Size-{product.sizeDTO.size}</p>
+          <div className="flex w-full objects-center">
+            <div className="flex justify-between items-center mt-2 md:mt-4 h-4 w-16">
+              <div className="flex h-10 w-fit bg-transparent mt-1">
                 <IconButton onClick={decrementHandler}>
-                  <span className='m-auto text-xs font-black text-primary'>
+                  <span className="m-auto text-xs font-black text-primary">
                     <Remove />
                   </span>
                 </IconButton>
                 <IconButton onClick={incrementHandler}>
-                  <span className='m-auto text-xs font-black text-primary'>
+                  <span className="m-auto text-xs font-black text-primary">
                     <Add />
                   </span>
                 </IconButton>
@@ -48,19 +48,20 @@ const CartItem = ({ id, product, qty }) => {
           </div>
         </div>
       </div>
-      <div className='text-sm md:text-right md:text-base w-full'>
-        <span className='font-medium text-black text-xl'>
-          ₹{((100 - product.discount) / 100) * product.total * qty}
+      <div className="text-sm md:text-right md:text-base w-full">
+        <span className="font-medium text-black text-xl">
+          ₹{Math.round(((100 - product.discount) / 100) * product.total * qty)}
         </span>
-        <div className='flex items-center space-x-4'>
+        <div className="flex items-center space-x-4">
           <h1>Quantity</h1>
           <h1>{qty}</h1>
         </div>
       </div>
-      <div className='absolute bottom-0 right-0'>
+      <div className="absolute bottom-0 right-0">
         <IconButton
           onClick={removeItem}
-          className='font-medium text-shadowGrey text-xl mx-4'>
+          className="font-medium text-shadowGrey text-xl mx-4"
+        >
           <Delete />
         </IconButton>
       </div>
--- src/components/cart-modal.js
+++ src/components/cart-modal.js
@@ -1,15 +1,15 @@
-import React, {useContext, useEffect} from "react";
-import {Modal, Text} from "@nextui-org/react";
+import React, { useContext, useEffect } from "react";
+import { Modal, Text } from "@nextui-org/react";
 import CartItem from "@/components/CartItem";
-import {UserContext} from "../../context/context";
-import {useRouter} from "next/router";
-import {isTokenValid} from "@/utils/JWTVerifier";
-import {uuidv4} from "@firebase/util";
-import Badge from '@mui/material/Badge';
-import {styled} from '@mui/material/styles';
-import IconButton from '@mui/material/IconButton';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import {Button} from "@mui/material";
+import { UserContext } from "../../context/context";
+import { useRouter } from "next/router";
+import { isTokenValid } from "@/utils/JWTVerifier";
+import { uuidv4 } from "@firebase/util";
+import Badge from "@mui/material/Badge";
+import { styled } from "@mui/material/styles";
+import IconButton from "@mui/material/IconButton";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import { Button } from "@mui/material";
 import { AddShoppingCart, Close } from "@mui/icons-material";
 import Link from "next/link";
 
@@ -51,46 +51,50 @@ export default function CartModal() {
 
   return (
     <div>
-      <IconButton aria-label='cart' onClick={handler}>
+      <IconButton aria-label="cart" onClick={handler}>
         <StyledBadge
           badgeContent={
             ctx.cart.cartItems !== undefined ? ctx.cart.cartItems.length : 0
-          }>
+          }
+        >
           <ShoppingCartIcon
             sx={{ color: "#FFF", height: "33px", width: "33px" }}
           />
         </StyledBadge>
       </IconButton>
       <Modal
-        scroll='false'
-        width='auto'
+        scroll="false"
+        width="auto"
         closeButton={false}
         className={
           "shadow-lg text-black bg-gray w-[80%] items-center mx-auto md:w-fit md:items-end"
         }
-        aria-labelledby='modal-title'
+        aria-labelledby="modal-title"
         open={visible}
-        onClose={closeHandler}>
+        onClose={closeHandler}
+      >
         <Modal.Header>
           <IconButton
-            className='absolute top-0 right-0 p-2 text-black'
-            onClick={closeHandler}>
+            className="absolute top-0 right-0 p-2 text-black"
+            onClick={closeHandler}
+          >
             <Close />
           </IconButton>
         </Modal.Header>
         {(ctx.cart.cartItems === undefined ||
           ctx.cart.cartItems.length === 0) && (
-          <Modal.Body className='w-full'>
-            <div className='flex flex-col justify-center text-center relative z-10 overflow-y-scroll'>
+          <Modal.Body className="w-full">
+            <div className="flex flex-col justify-center text-center relative z-10 overflow-y-scroll">
               <h1 className={"text-center font-quest text-3xl pb-10"}>
-                Your <span className='text-primary'>cart</span> is empty
+                Your <span className="text-primary">cart</span> is empty
               </h1>
-              <div className='flex flex-col space-y-3'>
-                <AddShoppingCart className='text-8xl text-primary mx-auto' />
+              <div className="flex flex-col space-y-3">
+                <AddShoppingCart className="text-8xl text-primary mx-auto" />
                 <Link
-                  href='/#merchandise'
-                  className='font-quest text-lg'
-                  onClick={closeHandler}>
+                  href="/#merchandise"
+                  className="font-quest text-lg"
+                  onClick={closeHandler}
+                >
                   Shop For More Products
                 </Link>
               </div>
@@ -100,12 +104,15 @@ export default function CartModal() {
         {ctx.cart.cartItems !== undefined &&
           ctx.cart.cartItems.length !== 0 && (
             <>
-              <Modal.Body className='md:w-full'>
-                <div className='p-5 rounded-lg font-quest'>
-                  <div className='w-full px-3 flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-8'>
-                    <div className='bg-white md:bg-gray flex flex-col md:border-none rounded md:w-8/12'>
+              <Modal.Body className="md:w-full">
+                <div className="p-5 rounded-lg font-quest">
+                  <div className="w-full px-3 flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-8">
+                    <div className="bg-white md:bg-gray flex flex-col md:border-none rounded md:w-8/12">
                       {ctx.cart.cartItems.map((item) => (
-                        <div key={uuidv4()} className='border-border border-b-2'>
+                        <div
+                          key={uuidv4()}
+                          className="border-border border-b-2"
+                        >
                           <CartItem
                             key={uuidv4()}
                             id={item.id}
@@ -115,31 +122,31 @@ export default function CartModal() {
                         </div>
                       ))}
                     </div>
-                    <div className='flex flex-col justify-between h-fit w-full md:w-4/12 p-4 bg-white rounded'>
-                      <div className='flex justify-between items-center'>
+                    <div className="flex flex-col justify-between h-fit w-full md:w-4/12 p-4 bg-white rounded">
+                      <div className="flex justify-between items-center">
                         <div>
-                          <span className='text-gray-600'>Subtotal</span>
+                          <span className="text-gray-600">Subtotal</span>
                         </div>
                         <div>
-                          <span className='font-medium text-2xl'>
-                            ₹{subTotal}
+                          <span className="font-medium text-2xl">
+                            ₹{Math.round(subTotal)}
                           </span>
                         </div>
                       </div>
-                      <hr className='h-px my-6 border-[#D9D9D9] border-1 '></hr>
-                      <div className='flex justify-between items-center'>
-                        <div className='w-1/2'>
-                          <span className='text-gray-600'>Total</span>
-                          <p className='text-xs text-black'>
+                      <hr className="h-px my-6 border-[#D9D9D9] border-1 "></hr>
+                      <div className="flex justify-between items-center">
+                        <div className="w-1/2">
+                          <span className="text-gray-600">Total</span>
+                          <p className="text-xs text-black">
                             Excluding taxes and charges
                           </p>
                         </div>
-                        <div className='w-1/2 text-right'>
-                          <span className='font-medium text-black text-sm'>
+                        <div className="w-1/2 text-right">
+                          <span className="font-medium text-black text-sm">
                             INR
                           </span>{" "}
-                          <span className='font-medium text-2xl'>
-                            ₹{subTotal}
+                          <span className="font-medium text-2xl">
+                            ₹{Math.round(subTotal)}
                           </span>
                         </div>
                       </div>
@@ -151,13 +158,15 @@ export default function CartModal() {
                 <Button
                   css={{ fontFamily: "$algeria" }}
                   style={{
-                    background: "linear-gradient(45deg, #ffa000 30%, #ffc107 90%)",
-                    color:"white",
+                    background:
+                      "linear-gradient(45deg, #ffa000 30%, #ffc107 90%)",
+                    color: "white",
                   }}
                   onClick={checkoutHandler}
                   variant={"contained"}
-                  color={'primary'}
-                  className='text-white font-prompt text-sm px-4 py-2'>
+                  color={"primary"}
+                  className="text-white font-prompt text-sm px-4 py-2"
+                >
                   Proceed to checkout
                 </Button>
               </Modal.Footer>
