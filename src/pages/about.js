644a6f818ddc4fb1f1cc7f6e514dee2c275e52ae
majestymewtwo <majestymewtwo@gmail.com>
2023-06-07 22:45:59 +0530

    trying commit again

--- src/pages/about.js
+++ src/pages/about.js
@@ -10,94 +10,102 @@ export default function About() {
         <link rel='icon' href='/logo.png' />
         <title>Madrasda</title>
       </Head>
+
       <ClientLayout>
         <main className='p-8 md:pt-20 w-full md:w-1/2 mx-auto'>
           <div className='font-quest text-base text-black flex flex-wrap justify-start items-center text-justify px-3 py-8 w-full'>
-            <p className='text-3xl text-left font-bold'>OUR USP</p> 
+            <p className='text-3xl text-left font-bold'>OUR USP</p>
             <br />
-            Today, I would like to introduce you to our exciting new
-            marketplace with product fulfilment services. Our platform is
-            designed to make selling and shipping products a seamless and
-            hassle-free experience for both sellers and buyers. 
-            <br /><br /><br />
+            Today, I would like to introduce you to our exciting new marketplace
+            with product fulfilment services. Our platform is designed to make
+            selling and shipping products a seamless and hassle-free experience
+            for both sellers and buyers.
+            <br />
+            <br />
+            <br />
             <p className='italic'>So, what is our marketplace all about?</p>
             <br />
-            Our platform is an online marketplace that
-            enables sellers to list their products for sale and provide
-            fulfilment services. We handle all aspects of the fulfillment
-            process, from storing inventory to packaging and shipping products
-            to customers.
-            <br /><br />
-            As a seller on our platform, you can focus on creating
-            and promoting your products while we take care of the rest. We
-            provide various tools and resources to help you manage your
-            inventory, track orders, and process payments.
-            <br /><br />
-            For buyers, our
-            platform provides a seamless and easy-to-use experience. You can
-            browse and purchase products from multiple sellers on our platform
-            and enjoy fast and reliable shipping.
-
-            <br /><br /><br />
-
+            Our platform is an online marketplace that enables sellers to list
+            their products for sale and provide fulfilment services. We handle
+            all aspects of the fulfillment process, from storing inventory to
+            packaging and shipping products to customers.
+            <br />
+            <br />
+            As a seller on our platform, you can focus on creating and promoting
+            your products while we take care of the rest. We provide various
+            tools and resources to help you manage your inventory, track orders,
+            and process payments.
+            <br />
+            <br />
+            For buyers, our platform provides a seamless and easy-to-use
+            experience. You can browse and purchase products from multiple
+            sellers on our platform and enjoy fast and reliable shipping.
+            <br />
+            <br />
+            <br />
             <p className='italic'>What sets our platform apart?</p>
             <br />
             We are not just a traditional marketplace. Our platform offers a
             unique and personalized approach that ensures both sellers and
             buyers are satisfied with the fulfillment process.
-            <br /><br />
-            We offer a variety of shipping options, including standard and expedited
-            shipping, to meet the needs of all customers. We also provide
-            tracking information for all orders, allowing buyers to stay
+            <br />
+            <br />
+            We offer a variety of shipping options, including standard and
+            expedited shipping, to meet the needs of all customers. We also
+            provide tracking information for all orders, allowing buyers to stay
             informed about the status of their purchase.
-            <br /><br />
-            Furthermore, our platform provides a marketing and promotion toolkit that helps
-            sellers reach a wider audience. We provide access to a community of
-            like-minded individuals, influencers, and potential customers, which
-            can help drive traffic to their stores and increase sales.
-            <br /><br />
-            In addition, our platform provides excellent customer support to both
-            sellers and buyers. Our team is always available to answer any
+            <br />
+            <br />
+            Furthermore, our platform provides a marketing and promotion toolkit
+            that helps sellers reach a wider audience. We provide access to a
+            community of like-minded individuals, influencers, and potential
+            customers, which can help drive traffic to their stores and increase
+            sales.
+            <br />
+            <br />
+            In addition, our platform provides excellent customer support to
+            both sellers and buyers. Our team is always available to answer any
             questions or concerns and to ensure that the fulfillment process
             runs smoothly.
-            <br /><br />
-            In conclusion, our marketplace with product
-            fulfillment services offers a unique opportunity for sellers to
-            focus on creating and promoting their products while we take care of
-            the rest. For buyers, we provide a seamless and easy-to-use
-            experience, with fast and reliable shipping. With our personalized
-            approach, seamless platform, and focus on value-added services, we
-            believe that we offer a valuable service to retailers.
-            
-            <br /><br /><br />
-            
-            <p className='italic'>Madras da for Production House</p> 
             <br />
-            Movie merchandise has been a significant part of
-            the film industry for decades. It's an excellent way for production
-            companies to extend their brand and make additional revenue from
-            their movies. In this blog, we'll discuss movie merchandise, the
-            benefits of selling it, and how much money production companies can
-            make by selling it.
-            <br /><br />
-            Movie merchandise refers to any product that has
-            been produced as a result of a film, such as action figures,
-            t-shirts, posters, and other memorabilia. It's a great way for fans
-            to connect with their favorite movies and characters beyond the film
-            itself.
-            <br /><br />
-            One of the primary benefits of selling movie merchandise is
-            that it can generate significant additional revenue for production
+            <br />
+            In conclusion, our marketplace with product fulfillment services
+            offers a unique opportunity for sellers to focus on creating and
+            promoting their products while we take care of the rest. For buyers,
+            we provide a seamless and easy-to-use experience, with fast and
+            reliable shipping. With our personalized approach, seamless
+            platform, and focus on value-added services, we believe that we
+            offer a valuable service to retailers.
+            <br />
+            <br />
+            <br />
+            <p className='italic'>Madras da for Production House</p>
+            <br />
+            Movie merchandise has been a significant part of the film industry
+            for decades. It's an excellent way for production companies to
+            extend their brand and make additional revenue from their movies. In
+            this blog, we'll discuss movie merchandise, the benefits of selling
+            it, and how much money production companies can make by selling it.
+            <br />
+            <br />
+            Movie merchandise refers to any product that has been produced as a
+            result of a film, such as action figures, t-shirts, posters, and
+            other memorabilia. It's a great way for fans to connect with their
+            favorite movies and characters beyond the film itself.
+            <br />
+            <br />
+            One of the primary benefits of selling movie merchandise is that it
+            can generate significant additional revenue for production
             companies. According to a report by the Licensing Industry
             Merchandisers Association (LIMA), licensed merchandise sales in
             North America alone reached $29.6 billion in 2019. This figure
             includes all forms of licensed merchandise, including those not
             related to movies, but it's still an indication of how much money
             can be made from merchandise sales.
-            
-            <br /><br /><br />
-            
-            <p className="italic">Traditional Way</p>
+            <br />
+            <br />
+            <br />
+            <p className='italic'>Traditional Way</p>
             <br />
             When a production company licenses its intellectual property to a
             merchandising partner, it earns a percentage of the revenue
@@ -105,54 +113,65 @@ export default function About() {
             but it's typically around 10-15% of the wholesale price of the
             product. So, if a t-shirt sells for $20, and the production
             company's percentage is 10%, they would earn $2 from each sale.
-            
-            <br /><br /><br />
-            
+            <br />
+            <br />
+            <br />
             <p className='italic'>What makes MADRAS DA unique</p>
             <br />
-            We have meticulously crafted a distinct
-            dashboard for Production Houses, facilitating them to set their own
-            margin with absolute transparency across all aspects.
-            <br /><br />
-            To provide an
-            illustration, if the base product value is 199, you have the freedom
-            to set the MRP at 399.
-            <br /><br />
-            This empowers the Production Houses to earn a
-            direct margin without the involvement of any intermediary, thereby
-            ensuring maximum profitability.
-            <br /><br />
-            In the event of 1000 products being
-            sold, with an average margin of 200 rupees per product, the
-            Production House stands to earn a substantial sum of 2 lakh rupees.
-            <br /><br />
+            We have meticulously crafted a distinct dashboard for Production
+            Houses, facilitating them to set their own margin with absolute
+            transparency across all aspects.
+            <br />
+            <br />
+            To provide an illustration, if the base product value is 199, you
+            have the freedom to set the MRP at 399.
+            <br />
+            <br />
+            This empowers the Production Houses to earn a direct margin without
+            the involvement of any intermediary, thereby ensuring maximum
+            profitability.
+            <br />
+            <br />
+            In the event of 1000 products being sold, with an average margin of
+            200 rupees per product, the Production House stands to earn a
+            substantial sum of 2 lakh rupees.
+            <br />
+            <br />
             The amount of money a production company can make from movie
             merchandise sales depends on several factors, such as the popularity
             of the movie, the types of products being sold, and the marketing
             efforts put into promoting the merchandise.
-            <br /><br />
-            For example, the Star
-            Wars franchise is one of the most successful movie merchandise
-            franchises of all time. According to a report by Statista, licensed
-            merchandise sales for Star Wars reached $3.3 billion in 2019. This
-            includes everything from action figures to clothing to video games.
-            <br /><br />
+            <br />
+            <br />
+            For example, the Star Wars franchise is one of the most successful
+            movie merchandise franchises of all time. According to a report by
+            Statista, licensed merchandise sales for Star Wars reached $3.3
+            billion in 2019. This includes everything from action figures to
+            clothing to video games.
+            <br />
+            <br />
             Other successful movie merchandise franchises include Marvel
             Cinematic Universe, Harry Potter, and Disney's animated movies.
             These franchises have generated billions of dollars in merchandise
             sales over the years.
-            <br /><br /><br />
-            In conclusion, movie merchandise is an
-            excellent way for production companies to extend their brand and
-            make additional revenue from their movies. The amount of money that
-            can be made from merchandise sales depends on several factors, but
-            it's clear that successful franchises can generate billions of
-            dollars in revenue. If you're a movie fan, buying merchandise can be
-            a great way to show your support for your favorite films and
-            characters while also contributing to the movie industry's success.
-            <br /><br /><br /><br />
+            <br />
+            <br />
+            <br />
+            In conclusion, movie merchandise is an excellent way for production
+            companies to extend their brand and make additional revenue from
+            their movies. The amount of money that can be made from merchandise
+            sales depends on several factors, but it's clear that successful
+            franchises can generate billions of dollars in revenue. If you're a
+            movie fan, buying merchandise can be a great way to show your
+            support for your favorite films and characters while also
+            contributing to the movie industry's success.
+            <br />
+            <br />
+            <br />
+            <br />
             <p className='text-center w-full font-bold'>
-              Welcome to MADRAS DA<br />
+              Welcome to MADRAS DA
+              <br />
               India's first marketplace for creators!
             </p>
           </div>
