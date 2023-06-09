6bcc21e78c49619b206d358049f7af628ba86ac9
majestymewtwo <majestymewtwo@gmail.com>
2023-06-09 20:28:01 +0530

    Added SEO and meta tags

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,94 +3,97 @@ import {Head, Html, Main, NextScript} from 'next/document'
 export default function Document() {
 
 	return (
-		<Html lang='en'>
-			<Head>
-				<link rel='preconnect' href='https://fonts.googleapis.com'/>
-				<link
-					rel='preconnect'
-					href='https://fonts.gstatic.com'
-					crossOrigin='true'
-				/>
-				<link
-					href='https://fonts.googleapis.com/css2?family=Alegreya+Sans:ital,wght@0,100;0,300;0,400;0,500;0,700;0,800;0,900;1,100;1,300;1,400;1,500;1,700;1,800;1,900&display=swap'
-					rel='stylesheet'
-				/>
-				{/* montsterrat */}
-				<link rel='preconnect' href='https://fonts.googleapis.com'/>
-				<link
-					rel='preconnect'
-					href='https://fonts.gstatic.com'
-					crossOrigin='true'
-				/>
-				<link
-					href='https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap'
-					rel='stylesheet'
-				/>
-				<link
-					href='https://fonts.googleapis.com/css2?family=Anuphan:wght@100;200;300;400;500;600;700&display=swap'
-					rel='stylesheet'
-				/>
-				<link rel='preconnect' href='https://fonts.googleapis.com'/>
-				<link
-					rel='preconnect'
-					href='https://fonts.gstatic.com'
-					crossOrigin='true'
-				/>
-				<link
-					href='https://fonts.googleapis.com/css2?family=Bubblegum+Sans&family=Ceviche+One&family=Galindo&family=Joti+One&display=swap'
-					rel='stylesheet'></link>
-				<link rel='preconnect' href='https://fonts.googleapis.com'/>
-				<link
-					rel='preconnect'
-					href='https://fonts.gstatic.com'
-					crossOrigin='true'
-				/>
-				<link
-					href='https://fonts.googleapis.com/css2?family=Bangers&display=swap'
-					rel='stylesheet'
-				/>
-				<link rel='preconnect' href='https://fonts.googleapis.com'/>
-				<link
-					rel='preconnect'
-					href='https://fonts.gstatic.com'
-					crossOrigin='true'
-				/>
-				<link
-					href='https://fonts.googleapis.com/css2?family=Londrina+Outline&display=swap'
-					rel='stylesheet'
-				/>
-				{/* nova flat */}
-				<link rel='preconnect' href='https://fonts.googleapis.com'/>
-				<link
-					rel='preconnect'
-					href='https://fonts.gstatic.com'
-					crossOrigin='true'
-				/>
-				<link
-					href='https://fonts.googleapis.com/css2?family=Nova+Flat&display=swap'
-					rel='stylesheet'
-				/>
-				<link
-					href='https://fonts.googleapis.com/css2?family=Prompt:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Questrial&family=Satisfy&display=swap'
-					rel='stylesheet'
-				/>
-				<link rel='preconnect' href='https://fonts.googleapis.com'/>
-				<link
-					rel='preconnect'
-					href='https://fonts.gstatic.com'
-					crossOrigin='true'
-				/>
-				<link
-					href='https://fonts.googleapis.com/css2?family=Rajdhani:wght@600&display=swap'
-					rel='stylesheet'
-				/>
-
-			</Head>
-			<body className='font-algeria'>
-			<Main/>
-			<NextScript/>
-			</body>
-
-		</Html>
-	);
+    <Html lang='en'>
+      <Head>
+        <link rel='preconnect' href='https://fonts.googleapis.com' />
+        <link
+          rel='preconnect'
+          href='https://fonts.gstatic.com'
+          crossOrigin='true'
+        />
+        <link
+          href='https://fonts.googleapis.com/css2?family=Alegreya+Sans:ital,wght@0,100;0,300;0,400;0,500;0,700;0,800;0,900;1,100;1,300;1,400;1,500;1,700;1,800;1,900&display=swap'
+          rel='stylesheet'
+        />
+        {/* montsterrat */}
+        <link rel='preconnect' href='https://fonts.googleapis.com' />
+        <link
+          rel='preconnect'
+          href='https://fonts.gstatic.com'
+          crossOrigin='true'
+        />
+        <link
+          href='https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap'
+          rel='stylesheet'
+        />
+        <link
+          href='https://fonts.googleapis.com/css2?family=Anuphan:wght@100;200;300;400;500;600;700&display=swap'
+          rel='stylesheet'
+        />
+        <link rel='preconnect' href='https://fonts.googleapis.com' />
+        <link
+          rel='preconnect'
+          href='https://fonts.gstatic.com'
+          crossOrigin='true'
+        />
+        <link
+          href='https://fonts.googleapis.com/css2?family=Bubblegum+Sans&family=Ceviche+One&family=Galindo&family=Joti+One&display=swap'
+          rel='stylesheet'></link>
+        <link rel='preconnect' href='https://fonts.googleapis.com' />
+        <link
+          rel='preconnect'
+          href='https://fonts.gstatic.com'
+          crossOrigin='true'
+        />
+        <link
+          href='https://fonts.googleapis.com/css2?family=Bangers&display=swap'
+          rel='stylesheet'
+        />
+        <link rel='preconnect' href='https://fonts.googleapis.com' />
+        <link
+          rel='preconnect'
+          href='https://fonts.gstatic.com'
+          crossOrigin='true'
+        />
+        <link
+          href='https://fonts.googleapis.com/css2?family=Londrina+Outline&display=swap'
+          rel='stylesheet'
+        />
+        {/* nova flat */}
+        <link rel='preconnect' href='https://fonts.googleapis.com' />
+        <link
+          rel='preconnect'
+          href='https://fonts.gstatic.com'
+          crossOrigin='true'
+        />
+        <link
+          href='https://fonts.googleapis.com/css2?family=Nova+Flat&display=swap'
+          rel='stylesheet'
+        />
+        <link
+          href='https://fonts.googleapis.com/css2?family=Prompt:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Questrial&family=Satisfy&display=swap'
+          rel='stylesheet'
+        />
+        <link rel='preconnect' href='https://fonts.googleapis.com' />
+        <link
+          rel='preconnect'
+          href='https://fonts.gstatic.com'
+          crossOrigin='true'
+        />
+        <link
+          href='https://fonts.googleapis.com/css2?family=Rajdhani:wght@600&display=swap'
+          rel='stylesheet'
+        />
+        <meta
+          name='description'
+          content="Madrasda is India's first content creators marketplace, providing a one-stop destination for official merchandise of your favorite content creators. Discover a diverse range of products from top Indian creators Shop now and get exclusive merchandise at Madrasda. Support your favorite creators"
+        />
+        <title>Madrasda | Official merchandise | Indian content creators</title>
+      </Head>
+      <body className='font-algeria'>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
