eb15a7990761f9401b5c09ddba07b885b1a6a532
majestymewtwo <majestymewtwo@gmail.com>
2023-06-10 21:01:09 +0530

    added '/creators/' alias to vendor products route

--- src/components/rightside-disc.js
+++ src/components/rightside-disc.js
@@ -8,23 +8,21 @@ export default function RightsideDisc({ name, id, imgUrl, products }) {
     <>
       {products.length !== 0 && (
         <div
-          className="hidden xl:block overflow-hidden pl-2
-                     lg:pl-10 mb-2 h-fit relative"
-        >
+          className='hidden xl:block overflow-hidden pl-2
+                     lg:pl-10 mb-2 h-fit relative'>
           <span
-            className="bg-secondary bg-opacity-95 flex flex-row justify-center items-center w-full
-                        rounded-l-full rounded-r-none"
-          >
-            <div className="relative">
+            className='bg-secondary bg-opacity-95 flex flex-row justify-center items-center w-full
+                        rounded-l-full rounded-r-none'>
+            <div className='relative'>
               <Image
-                className="animate-spin"
-                src="/disc.png"
+                className='animate-spin'
+                src='/disc.png'
                 width={500}
                 height={500}
               />
-              <div className="w-[250px] h-[250px] overflow-hidden absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-cover objects-center rounded-full">
+              <div className='w-[250px] h-[250px] overflow-hidden absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-cover objects-center rounded-full'>
                 <Image
-                  className="object-cover w-full h-full animate-spin hover:animate-none"
+                  className='object-cover w-full h-full animate-spin hover:animate-none'
                   alt={name}
                   src={imgUrl || "/logo.png"}
                   width={500}
@@ -33,18 +31,22 @@ export default function RightsideDisc({ name, id, imgUrl, products }) {
               </div>
             </div>
 
-            <div className="flex flex-col w-full">
-              <Link href={`/products/${id}`}>
-                <h1 className="font-prompt italic font-black text-2xl tracking-widest text-logo flex justify-end pr-10 text-center my-3 hover:my-2 hover:text-4xl transition-all ease-in-out duration-500 top-0 right-0">
+            <div className='flex flex-col w-full'>
+              <Link
+                href={{
+                  pathname: "/products/[vendorId]",
+                  query: { vendorId: id },
+                }}
+                as={"creators/" + name}>
+                <h1 className='font-prompt italic font-black text-2xl tracking-widest text-logo flex justify-end pr-10 text-center my-3 hover:my-2 hover:text-4xl transition-all ease-in-out duration-500 top-0 right-0'>
                   {name}
                 </h1>
               </Link>
 
               {/* -------- VISIBLE ONLY ON LARGE SCREENS --------  */}
               <div
-                className="w-full h-full items-center justify-start px-4 py-2 hidden
-                            xl:flex"
-              >
+                className='w-full h-full items-center justify-start px-4 py-2 hidden
+                            xl:flex'>
                 {products &&
                   products.map((prod) => {
                     if (prod.colors.length === 0) return null;
@@ -87,10 +89,15 @@ export default function RightsideDisc({ name, id, imgUrl, products }) {
                   })}
 
                 {products.length === 4 && (
-                  <div className="mr-5 z-10 relative ">
-                    <Link href={`/products/${id}`}>
+                  <div className='mr-5 z-10 relative '>
+                    <Link
+                      href={{
+                        pathname: "/products/[vendorId]",
+                        query: { vendorId: id },
+                      }}
+                      as={"creators/" + name}>
                       <Image
-                        src="/prod-comp-showmore-right.png"
+                        src='/prod-comp-showmore-right.png'
                         width={50}
                         height={50}
                       />
@@ -104,19 +111,24 @@ export default function RightsideDisc({ name, id, imgUrl, products }) {
       )}
       {products.length !== 0 && (
         <>
-          <div className="xl:hidden">
-            <div className="flex justify-between items-center mt-10">
-              <div className="absolute mt-10">
-                <Link href={`products/${id}`} className="relative">
+          <div className='xl:hidden'>
+            <div className='flex justify-between items-center mt-10'>
+              <div className='absolute mt-10'>
+                <Link
+                  href={{
+                    pathname: "/products/[vendorId]",
+                    query: { vendorId: id },
+                  }}
+                  as={"creators/" + name} className="relative">
                   <Image
-                    className="mx-auto md:mx-2 md:animate-spin hidden xl:block"
-                    src="/disc.png"
+                    className='mx-auto md:mx-2 md:animate-spin hidden xl:block'
+                    src='/disc.png'
                     width={170}
                     height={170}
                   />
-                  <div className="w-[140px] h-[140px] overflow-hidden absolute -top-[70px] left-[5px] xl:top-1/2 xl:left-1/2 xl:transform xl:-translate-x-1/2 xl:-translate-y-1/2 bg-cover objects-center rounded-full">
+                  <div className='w-[140px] h-[140px] overflow-hidden absolute -top-[70px] left-[5px] xl:top-1/2 xl:left-1/2 xl:transform xl:-translate-x-1/2 xl:-translate-y-1/2 bg-cover objects-center rounded-full'>
                     <Image
-                      className="object-cover w-full h-full border-8 border-black xl:border-none rounded-full"
+                      className='object-cover w-full h-full border-8 border-black xl:border-none rounded-full'
                       alt={name}
                       src={imgUrl || "/logo.png"}
                       width={200}
@@ -125,15 +137,15 @@ export default function RightsideDisc({ name, id, imgUrl, products }) {
                   </div>
                 </Link>
               </div>
-              <div className="flex flex-col w-full">
-                <h1 className="md:text-2xl text-lg font-raj text-right w-full text-primary pr-4">
+              <div className='flex flex-col w-full'>
+                <h1 className='md:text-2xl text-lg font-raj text-right w-full text-primary pr-4'>
                   {name}
                 </h1>
               </div>
             </div>
           </div>
-          <span className="flex flex-col xl:hidden bg-bg w-full mx-auto pt-20">
-            <div className="grid grid-cols-2 md:grid-cols-4 md:px-2">
+          <span className='flex flex-col xl:hidden bg-bg w-full mx-auto pt-20'>
+            <div className='grid grid-cols-2 md:grid-cols-4 md:px-2'>
               {/* flex flex-row overflow-hidden  */}
               {products &&
                 products.map((prod) => {
@@ -141,33 +153,32 @@ export default function RightsideDisc({ name, id, imgUrl, products }) {
                   return (
                     <div
                       key={uuidv4()}
-                      className="row-span-1 font-quest p-4 cursor-pointer border border-gray bg-tertiary m-2 rounded-sm"
-                    >
+                      className='row-span-1 font-quest p-4 cursor-pointer border border-gray bg-tertiary m-2 rounded-sm'>
                       <Link href={`/productDetails/${prod.id}`}>
-                        <div className="block relative h-36 rounded overflow-hidden">
+                        <div className='block relative h-36 rounded overflow-hidden'>
                           <Image
                             src={prod.colors[0].images[0]}
-                            alt="ecommerce"
+                            alt='ecommerce'
                             width={1080}
                             height={1920}
-                            className="object-contain object-center w-full h-full block"
+                            className='object-contain object-center w-full h-full block'
                           />
                         </div>
-                        <div className="mt-4 flex flex-col">
-                          <h2 className="text-black title-font text-[15px] md:text-[20px] xl:text-lg font-medium">
+                        <div className='mt-4 flex flex-col'>
+                          <h2 className='text-black title-font text-[15px] md:text-[20px] xl:text-lg font-medium'>
                             {prod.name}
                           </h2>
-                          <span className="mt-1 text-black text-[14px] md:text-xl">
+                          <span className='mt-1 text-black text-[14px] md:text-xl'>
                             ₹
                             {Math.round(
                               prod.total - prod.total * prod.discount * 0.01
                             )}
                           </span>
-                          <div className="flex justify-between">
-                            <span className="mt-1 line-through text-sm text-black">
+                          <div className='flex justify-between'>
+                            <span className='mt-1 line-through text-sm text-black'>
                               ₹{prod.total}
                             </span>
-                            <span className="title-font font-semibold text-sm text-[#088240]">
+                            <span className='title-font font-semibold text-sm text-[#088240]'>
                               {prod.discount}% off
                             </span>
                           </div>
