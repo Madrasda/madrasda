902f498fc7a88ae66049e428baaf30290b46ee44
majestymewtwo <majestymewtwo@gmail.com>
2023-06-18 11:27:49 +0530

    added shipping charges to billing invoice

--- src/components/orderdetails-modal.js
+++ src/components/orderdetails-modal.js
@@ -42,6 +42,18 @@ export default function OrderDetailsModal({ order }) {
       });
   };
 
+  useEffect(() => {
+    var total = 0;
+    order.orderItems.forEach((item) => {
+      const price =
+        Math.ceil(
+          (Math.ceil(100 - item.product.discount) / 100) * item.product.total
+        ) * item.quantity;
+      total += price;
+    });
+    setProdTotal(total);
+  }, []);
+
   return (
     <>
       <Button
@@ -50,46 +62,50 @@ export default function OrderDetailsModal({ order }) {
           background: "linear-gradient(45deg, #ffa000 30%, #ffc107 90%)",
         }}
         onClick={handler}
-        variant={"contained"}
-      >
+        variant={"contained"}>
         View Billing Info
       </Button>
       <div>
         <Modal
-          width="1080px"
+          width='1080px'
           closeButton
           preventClose
-          aria-labelledby="modal-title"
+          aria-labelledby='modal-title'
           open={visible}
           onClose={closeHandler}
-          className="w-[90%] md:w-[70%] mx-auto"
-          css={{ fontFamily: "$algeria" }}
-        >
-          <Modal.Body className="font-quest">
+          className='w-[90%] md:w-[70%] mx-auto'
+          css={{ fontFamily: "$algeria" }}>
+          <Modal.Body className='font-quest'>
             <div
-              className="bg-white shadow-lg overflow-hidden w-full mx-auto"
-              id={`${order.id}`}
-            >
+              className='bg-white shadow-lg overflow-hidden w-full mx-auto'
+              id={`${order.id}`}>
               {/* <!-- Order header --> */}
-              <div className="bg-bg text-white py-4 px-3 flex justify-between items-center w-full">
-                <h1 className="md:text-lg w-fit font-semibold">#{order.orderId}</h1>
-                <span className="md:text-lg w-fit">
+              <div className='bg-bg text-white py-4 px-3 flex justify-between items-center w-full'>
+                <h1 className='md:text-lg w-fit font-semibold'>
+                  #{order.orderId}
+                </h1>
+                <span className='md:text-lg w-fit'>
                   ₹{Number(order.orderTotal).toLocaleString("en-IN")}
                 </span>
               </div>
-              <div className="p-2 md:p-4">
+              <div className='p-2 md:p-4'>
+                <div className='flex justify-between items-center mb-4'>
+                  <h1>Shipping Charges</h1>
+                  <h1 className='text-sm text-black'>
+                    ₹{Math.round((order.orderTotal - prodTotal) * 100) / 100}
+                  </h1>
+                </div>
                 {order.orderItems.map((item) => (
                   <div
                     key={uuidv4()}
-                    className="flex justify-between items-center mb-4"
-                  >
-                    <span className="flex w-full space-x-2 items-center md:text-lg text-black font-medium">
+                    className='flex justify-between items-center mb-4'>
+                    <span className='flex w-full space-x-2 items-center md:text-lg text-black font-medium'>
                       <h1>{item.product.name}</h1>
-                      <h1 className="text-sm text-shadowGrey">
+                      <h1 className='text-sm text-shadowGrey'>
                         - Quantity {item.quantity}
                       </h1>
                     </span>
-                    <span className="text-black">
+                    <span className='text-black'>
                       ₹
                       {Number(
                         ((item.product.total * (100 - item.product.discount)) /
@@ -99,46 +115,46 @@ export default function OrderDetailsModal({ order }) {
                     </span>
                   </div>
                 ))}
-                <hr className="my-6" />
+                <hr className='my-6' />
                 {/* <!-- Billing information --> */}
-                <div className="flex justify-between w-full">
-                  <h1 className="md:text-lg font-medium mb-4">Order Date</h1>
-                  <h1 className="text-shadowGrey">
+                <div className='flex justify-between w-full'>
+                  <h1 className='md:text-lg font-medium mb-4'>Order Date</h1>
+                  <h1 className='text-shadowGrey'>
                     {order.orderDate.substr(0, 10)}
                   </h1>
                 </div>
-                <div className="flex justify-between w-full">
-                  <h1 className="md:text-lg font-medium mb-4">Payment ID</h1>
-                  <h1 className="text-shadowGrey">{order.paymentId}</h1>
+                <div className='flex justify-between w-full'>
+                  <h1 className='md:text-lg font-medium mb-4'>Payment ID</h1>
+                  <h1 className='text-shadowGrey'>{order.paymentId}</h1>
                 </div>
-                <h2 className="md:text-lg font-bold mb-4">Shipping Address</h2>
-                <div className="grid grid-cols-2 gap-4 w-full">
+                <h2 className='md:text-lg font-bold mb-4'>Shipping Address</h2>
+                <div className='grid grid-cols-2 gap-4 w-full'>
                   <div>
-                    <span className="text-black">Name</span>
-                    <p className="font-medium">{order.shippingAddress.name}</p>
+                    <span className='text-black'>Name</span>
+                    <p className='font-medium'>{order.shippingAddress.name}</p>
                   </div>
                   <div>
-                    <span className="text-black">Phone</span>
-                    <p className="font-medium">{order.shippingAddress.phone}</p>
+                    <span className='text-black'>Phone</span>
+                    <p className='font-medium'>{order.shippingAddress.phone}</p>
                   </div>
                   <div>
-                    <span className="text-black">Email</span>
-                    <p className="font-medium">{order.shippingAddress.email}</p>
+                    <span className='text-black'>Email</span>
+                    <p className='font-medium'>{order.shippingAddress.email}</p>
                   </div>
                   <div>
-                    <span className="text-black">Address</span>
-                    <p className="font-medium">
+                    <span className='text-black'>Address</span>
+                    <p className='font-medium'>
                       {order.shippingAddress.addressLine1}
                     </p>
-                    <p className="font-medium">
+                    <p className='font-medium'>
                       {order.shippingAddress.addressLine2}
                     </p>
-                    <p className="font-medium">{order.shippingAddress.city}</p>
-                    <p className="font-medium">{order.shippingAddress.state}</p>
-                    <p className="font-medium">
+                    <p className='font-medium'>{order.shippingAddress.city}</p>
+                    <p className='font-medium'>{order.shippingAddress.state}</p>
+                    <p className='font-medium'>
                       {order.shippingAddress.postalCode}
                     </p>
-                    <p className="font-medium">
+                    <p className='font-medium'>
                       {order.shippingAddress.country}
                     </p>
                   </div>
@@ -152,8 +168,7 @@ export default function OrderDetailsModal({ order }) {
                 color: "white",
               }}
               variant={"outlined"}
-              onClick={downloadInvoice}
-            >
+              onClick={downloadInvoice}>
               <Download /> Billing Invoice
             </Button>
           </Modal.Body>
