a6980b9ba732c50ff5c2fc69371d41cfa6f083a2
PughalBot <77720496+PughalBot@users.noreply.github.com>
2023-06-30 18:48:38 +0530

    fb pixel inserted

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -91,6 +91,30 @@ export default function Document() {
         />
 				<meta name="description" content="Madrasda is India's first content creators marketplace, providing a one-stop destination for official merchandise of your favorite content creators. Discover a diverse range of products from top Indian creators Shop now and get exclusive merchandise at Madrasda."
 				/>
+        <script
+    dangerouslySetInnerHTML={{
+      __html: `
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)}(window, document,'script',
+        'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '865965204469945');
+        fbq('track', 'PageView');
+      `
+    }}
+  />
+  <noscript>
+    <img
+      height="1"
+      width="1"
+      style={{ display: 'none' }}
+      src="https://www.facebook.com/tr?id=865965204469945&ev=PageView&noscript=1"
+    />
+  </noscript>
       </Head>
       <body className='font-algeria'>
         <Main />
