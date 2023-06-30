ad1e72f5fefdaa17aaf9c1bc86ac8f6ba9db17ca
jolanjose <jolanjose2003@gmail.com>
2023-06-30 07:38:39 +0530

    Add files via upload

---
+++ src/pages/creators/[vendorId].js
@@ -0,0 +1,52 @@
+import {useRouter} from 'next/router';
+import ProductList from "@/pages/productlist";
+import {useContext, useEffect, useState} from "react";
+import axios from "axios";
+import {UserContext} from "../../../context/context";
+
+function VendorProductsPage() {
+    const router = useRouter();
+    const ctx = useContext(UserContext);
+    const {vendorId} = router.query;
+    console.log(vendorId);
+    // Fetch the products for the specified vendorId
+    const [vendorProducts, setVendorProducts] = useState([]);
+    const [pageNo, setPageNo] = useState(0);
+    const [title, setTitle] = useState("")
+    const [vendorList, setVendorList] = useState([]);
+
+    const setPageData = (vendors) => {
+        setTitle(
+          vendors.find(
+            (vendor) => vendor.id === parseInt(router.query.vendorId)
+          ).name
+        );
+        axios.get("https://spring-madrasda-2f6mra4vwa-em.a.run.app/api/product/getProductsByVendor/" + vendorId + "?pageNo=" + pageNo + "&pageSize=20")
+            .then(response => {
+                setVendorProducts(response.data); 
+                console.log(response.data)
+            })
+            .catch(err => console.log(err));
+    }
+    
+    useEffect(() => {
+        if (router.isReady) {
+
+            if ((ctx.vendorList.length !== 0 && ctx.vendorList !== undefined)) {
+                setPageData(ctx.vendorList);
+            } else {
+                axios.get("https://spring-madrasda-2f6mra4vwa-em.a.run.app/api/admin/getVendors")
+                    .then(response => {
+                        setPageData(response.data)
+                        console.log(response.data);
+                    })
+                    .catch(err => console.log(err));
+            }
+        }
+    }, [vendorId, pageNo]);
+
+
+    return <ProductList productsPage={vendorProducts} setPageNo={setPageNo} pageNo={pageNo} title={title}/>;
+}
+
+export default VendorProductsPage;
